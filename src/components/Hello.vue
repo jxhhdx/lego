e863c6045116b0985930b51a8ec98b15fc540c9e
gxuud <igaoxian@foxmail.com>
2021-12-23 20:48:01 +0800

    feat: 添加单元测试

---
+++ src/components/Hello.vue
@@ -0,0 +1,18 @@
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'HelloWorld',
+  props: {
+    msg: String,
+  },
+});
+</script>
+
+<style>
+
+</style>
--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,12 +1,38 @@
-import { shallowMount } from '@vue/test-utils'
+import { mount } from '@vue/test-utils'
 import HelloWorld from '@/components/HelloWorld.vue'
+import Hello from '@/components/Hello.vue'
 
 describe('HelloWorld.vue', () => {
   it('renders props.msg when passed', () => {
     const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
+    const wrapper = mount(HelloWorld, {
       props: { msg }
     })
-    expect(wrapper.text()).toMatch(msg)
+    console.log(wrapper.findComponent(Hello).props());
+    console.log(wrapper.get('h1').text());
+  })
+  it('should update the count when clicking thne button', async () => {
+    const msg = 'new message'
+    const wrapper = mount(HelloWorld, {
+      props: { msg }
+    })
+    await wrapper.get('button').trigger('click')
+    expect(wrapper.get('button').text()).toBe('1')
+  })
+  it('should update the count when fill the input add click the add button', async () => {
+    const msg = 'new message'
+    const todoContent = 'buy milk'
+    const wrapper = mount(HelloWorld, {
+      props: { msg }
+    })
+    await wrapper.get('input').setValue(todoContent)
+    expect(wrapper.get('input').element.value).toBe(todoContent)
+    await wrapper.get('.addTodo').trigger('click')
+    expect(wrapper.findAll('li')).toHaveLength(1)
+    expect(wrapper.get('li').text()).toBe(todoContent)
+    const events = wrapper.emitted('send') ?? []
+    console.log(events);
+    
+    expect(events[0]).toEqual([todoContent])
   })
 })
